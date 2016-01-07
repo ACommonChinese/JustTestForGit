3b6de6c5e407d90f9b1577b30b899567cbbd7814
刘威振 <liuweizhen@liuweizhendeMacBook-Pro.local>
2016-01-07 16:41:42 +0800

    Test

---
+++ Demo/Demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by 刘威振 on 16/1/7.
+//  Copyright © 2016年 LiuWeiZhen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
